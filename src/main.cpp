04ee935b876141db48d6c30dfde5c4276ac778f1
joao eger <egerjoaoarnoldo@gmail.com>
2026-09-22 23:50:55 -0300

    add cpi report

--- src/main.cpp
+++ src/main.cpp
@@ -30,4 +30,6 @@ int main(int argc, char *argv[]) {
     for (const Instruction &inst : instrucoes) {
         cout << formatarLinhaSaida(inst) << "\n";
     }
+
+    cout << relatorioCPI(instrucoes) << "\n";
 }
--- src/report.cpp
+++ src/report.cpp
@@ -1,5 +1,6 @@
 #include "converter.hpp"
 #include "report.hpp"
+#include "cpi_calculator.hpp"
 
 string formatarLinhaSaida(const Instruction &inst) {
     ostringstream out;
@@ -15,6 +16,30 @@ string formatarLinhaSaida(const Instruction &inst) {
 }
 
 string relatorioCPI(const vector<Instruction> &instrucoes) {
-    //a fazer
-    return "";
+    double cpiMedio = calcularCPIMedio(instrucoes);
+    int totalInstrucoes = instrucoes.size();
+    int totalInstR = 0, totalInstI = 0, totalInstS = 0, totalInstB = 0, totalInstU = 0, totalInstJ = 0;
+    for (auto &inst : instrucoes) {
+        if (inst.type == "R") totalInstR++;
+        else if (inst.type == "I") totalInstI++;
+        else if (inst.type == "S") totalInstS++;
+        else if (inst.type == "B") totalInstB++;
+        else if (inst.type == "U") totalInstU++;
+        else if (inst.type == "J") totalInstJ++;
+    }
+    auto percentual = [totalInstrucoes](int quantidade) {
+        double percentual = totalInstrucoes == 0 ? 0.0 : (100.0 * quantidade / totalInstrucoes);
+        ostringstream out;
+        out << fixed << setprecision(1) << percentual << "%";
+        return out.str();
+    };
+
+    return "Total de Instruções: " + to_string(totalInstrucoes) + "\n" +
+           "CPI Médio: " + to_string(cpiMedio) + "\n" +
+           "Instruções R-type: " + to_string(totalInstR) + " (" + percentual(totalInstR) + ")\n" +
+           "Instruções I-type: " + to_string(totalInstI) + " (" + percentual(totalInstI) + ")\n" +
+           "Instruções S-type: " + to_string(totalInstS) + " (" + percentual(totalInstS) + ")\n" +
+           "Instruções B-type: " + to_string(totalInstB) + " (" + percentual(totalInstB) + ")\n" +
+           "Instruções U-type: " + to_string(totalInstU) + " (" + percentual(totalInstU) + ")\n" +
+           "Instruções J-type: " + to_string(totalInstJ) + " (" + percentual(totalInstJ) + ")";
 }
